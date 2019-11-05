682e34884c30f676c6bebf69659dc3171de8c3be
pedago <pedago@pcalb-eb0209.univ-jfc.fr>
2019-11-05 14:53:35 +0100

    Ajout constructeur pour la connexion

--- src/main/java/com/mycompany/tpjsp/DAO.java
+++ src/main/java/com/mycompany/tpjsp/DAO.java
@@ -4,11 +4,27 @@
  * and open the template in the editor.
  */
 package com.mycompany.tpjsp;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
 
 /**
  *
  * @author pedago
  */
 public class DAO {
+    private final DataSource mySource;
+    
+    public DAO(DataSource datasource) {
+        this.mySource = datasource;
+    }
+    
     
 }
