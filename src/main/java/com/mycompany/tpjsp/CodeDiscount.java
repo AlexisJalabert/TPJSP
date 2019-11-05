55d4b44467525d2c2023b0c4578ee84292d51f07
pedago <pedago@pcalb-eb0209.univ-jfc.fr>
2019-11-05 14:56:38 +0100

    Création du constructeur et des getters

--- src/main/java/com/mycompany/tpjsp/CodeDiscount.java
+++ src/main/java/com/mycompany/tpjsp/CodeDiscount.java
@@ -9,9 +9,24 @@ package com.mycompany.tpjsp;
 
 
 /**
- *
+ * Un enregistrement de la table DISCOUNT_CODE
  * @author pedago
  */
 public class CodeDiscount {
+    private String code;
     
+    private float taux;
+    
+    public CodeDiscount(String codeC, float tauxC) {
+        this.code = codeC;
+        this.taux = tauxC;
+    }
+    
+    public String getCodeDiscount() {
+        return code;
+    }
+    
+    public float getTauxDiscount() {
+        return taux;
+    }
 }
